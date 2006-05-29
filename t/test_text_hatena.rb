27ed3e63e8c13b9cf7a22e179e05f634f1ef95a9
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2006-05-29 23:42:23 +0000

    * t/test_text_hatena.rb: Text::Hatena 0.12 への対応中。

--- t/test_text_hatena.rb
+++ t/test_text_hatena.rb
@@ -23,7 +23,7 @@ body
 END
     html2 = <<END
 <div class="section">
-\t<h3><a name="p1" href="http://d.hatena.ne.jp/jkondo/20050906#p1"><span class="sanchor">sa</span></a> title</h3>
+\t<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#p1" name="p1"><span class="sanchor">sa</span></a> title</h3>
 \t<p>body</p>
 </div>
 END
@@ -39,7 +39,7 @@ remember time.
 END
     html2 = <<END
 <div class="section">
-\t<h3><a name="1234567890" href="http://d.hatena.ne.jp/jkondo/20050906#1234567890"><span class="sanchor">sa</span></a> record time</h3> <span class="timestamp">08:31</span>
+\t<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#1234567890" name="1234567890"><span class="sanchor">sa</span></a> record time</h3> <span class="timestamp">08:31</span>
 \t<p>remember time.</p>
 </div>
 END
@@ -56,7 +56,7 @@ I like this.
 END
     html2 = <<END
 <div class="section">
-\t<h3><a name="hobby" href="http://d.hatena.ne.jp/jkondo/20050906#hobby"><span class="sanchor">sa</span></a> [<a href="http://d.hatena.ne.jp/jkondo/?word=hobby" class="sectioncategory">hobby</a>]my hobby</h3>
+\t<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#hobby" name="hobby"><span class="sanchor">sa</span></a> [<a href="http://d.hatena.ne.jp/jkondo/?word=hobby" class="sectioncategory">hobby</a>]my hobby</h3>
 \t<p>I like this.</p>
 </div>
 END
@@ -165,6 +165,33 @@ END
     html2.chomp!
     assert_equal(html2, html)
 
+    # ul2
+    text = <<END
+-komono
+--kyoto
+---shibuya
+--hachiyama
+END
+    @p.parse(text)
+    html = @p.html
+
+    html2 = <<END
+<div class="section">
+<div class="section">
+\t<ul>
+\t\t<li>komono
+\t\t<ul>
+\t\t\t<li>kyoto
+\t\t\t<ul>
+\t\t\t\t<li>shibuya</li>
+\t\t\t</ul>
+\t\t\t</li>
+\t\t\t<li>hachiyama</li>
+\t\t</ul>
+\t\t</li>
+\t</ul>
+</div>
+END
 
     # ol
     text = <<END
@@ -212,7 +239,7 @@ html starts with <html>.
 END
     html2 = <<END
 <div class="section">
-\t<pre>
+\t<pre class="hatena-super-pre">
 html starts with &lt;html&gt;.
 </pre>
 </div>
@@ -286,13 +313,36 @@ END
     html2.chomp!
     assert_equal(html2, html)
 
+    text = <<END
+Here is the way to make link.
+>||
+http://www.hatena.ne.jp/
+id:jkondo
+||<
+bye.
+END
+    html2 = <<END
+<div class="section">
+\t<p>Here is the way to make link.</p>
+\t<pre class="hatena-super-pre">
+http://www.hatena.ne.jp/
+id:jkondo
+</pre>
+\t<p>bye.</p>
+</div>
+END
+    @p.parse(text)
+    html = @p.html
+    html2.chomp!
+    assert_equal(html2, html)
+
     # footnote
     text = <<END
 GNU((GNU Is Not Unix)) is not unix.
 END
     html2 = <<END
 <div class="section">
-\t<p>GNU<span class="footnote"><a name="fn1" href="http://d.hatena.ne.jp/jkondo/20050906#f1" title="GNU Is Not Unix">*1</a></span> is not unix.</p>
+\t<p>GNU<span class="footnote"><a href="http://d.hatena.ne.jp/jkondo/20050906#f1" title="GNU Is Not Unix" name="fn1">*1</a></span> is not unix.</p>
 </div>
 <div class="footnote">
 \t<p class="footnote"><a href="http://d.hatena.ne.jp/jkondo/20050906#fn1" name="f1">*1</a>: GNU Is Not Unix</p>
