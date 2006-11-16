8d3db8438daf9f533f3c33d106c541ed48b414e4
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2006-11-16 19:12:48 +0000

    * t/test_text_hatena.rb: Test::Baseを使った。

--- t/test_text_hatena.rb
+++ t/test_text_hatena.rb
@@ -1,355 +1,251 @@
-require "test/unit"
-require "text/hatena"
+require "t/test_helper"
 
-class Test_Text_Hatena < Test::Unit::TestCase
-  def setup
-    @base = "http://d.hatena.ne.jp/jkondo/"
-    @perma = "http://d.hatena.ne.jp/jkondo/20050906"
-    @sa = "sa"
-    @p = Text::Hatena.new({ :baseuri => @base,
-                            :permalink => @perma,
-                            :ilevel => 0,
-                            :invalidnode => [],
-                            :sectionanchor => @sa })
+class Test::Base
+  PASSTHROUGH_EXCEPTIONS = []
+end
+
+class TextHatenaTest < Test::Base
+  filters %w(.strip)
+  filters :in => "hatenaize"
+  delimitors '===', ';;;'
+  run_equal :out, :in
+
+  def hatenaize(value)
+    parser = Text::Hatena.new({ :baseuri => "http://d.hatena.ne.jp/jkondo/",
+                                :permalink => "http://d.hatena.ne.jp/jkondo/20050906",
+                                :ilevel => 0,
+                                :invalidnode => [],
+                                :sectionanchor => "sa" })
+    parser.parse(value)
+    parser.html
   end
+end
 
-  def test_all
-    assert_instance_of(Text::Hatena, @p)
-
-    # h3
-    text = <<END
+__END__
+===
+;;; in
 *title
 body
-END
-    html2 = <<END
-<div class="section">
-\t<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#p1" name="p1"><span class="sanchor">sa</span></a> title</h3>
-\t<p>body</p>
-</div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    # h3time
-    text = <<END
+;;; out
+<div class="section">
+	<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#p1" name="p1"><span class="sanchor">sa</span></a> title</h3>
+	<p>body</p>
+</div>
+
+===
+;;; in
 *1234567890*record time
 remember time.
-END
-    html2 = <<END
+
+;;; out
 <div class="section">
-\t<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#1234567890" name="1234567890"><span class="sanchor">sa</span></a> record time</h3> <span class="timestamp">08:31</span>
-\t<p>remember time.</p>
+	<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#1234567890" name="1234567890"><span class="sanchor">sa</span></a> record time</h3> <span class="timestamp">08:31</span>
+	<p>remember time.</p>
 </div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-
-    # h3cat
-    text = <<END
+===
+;;; in
 *hobby*[hobby]my hobby
 I like this.
-END
-    html2 = <<END
+
+;;; out
 <div class="section">
-\t<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#hobby" name="hobby"><span class="sanchor">sa</span></a> [<a href="http://d.hatena.ne.jp/jkondo/?word=hobby" class="sectioncategory">hobby</a>]my hobby</h3>
-\t<p>I like this.</p>
+	<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#hobby" name="hobby"><span class="sanchor">sa</span></a> [<a href="http://d.hatena.ne.jp/jkondo/?word=hobby" class="sectioncategory">hobby</a>]my hobby</h3>
+	<p>I like this.</p>
 </div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-
-    # h4
-    text = <<END;
+===
+;;; in
 **h4title
 
 h4body
-END
-    html2 = <<END
-<div class="section">
-\t<h4>h4title</h4>
-\t
-\t<p>h4body</p>
-</div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    # h5
-    text = <<END
+;;; out
+<div class="section">
+	<h4>h4title</h4>
+	
+	<p>h4body</p>
+</div>
+
+===
+;;; in
 ***h5title
 
 h5body
-END
-    html2 = <<END
-<div class="section">
-\t<h5>h5title</h5>
-\t
-\t<p>h5body</p>
-</div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    # blockquote
-    text = <<END
+;;; out
+<div class="section">
+	<h5>h5title</h5>
+	
+	<p>h5body</p>
+</div>
+
+===
+;;; in
 >>
 quoted
 <<
-END
-    html2 = <<END
-<div class="section">
-\t<blockquote>
-\t\t<p>quoted</p>
-\t</blockquote>
-</div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    # dl
-    text = <<END
+;;; out
+<div class="section">
+	<blockquote>
+		<p>quoted</p>
+	</blockquote>
+</div>
+
+===
+;;; in
 :cinnamon:dog
-END
-    html2 = <<END
-<div class="section">
-\t<dl>
-\t\t<dt>cinnamon</dt>
-\t\t<dd>dog</dd>
-\t</dl>
-</div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    # ul
-    text = <<END
+;;; out
+<div class="section">
+	<dl>
+		<dt>cinnamon</dt>
+		<dd>dog</dd>
+	</dl>
+</div>
+
+===
+;;; in
 -komono
 --kyoto
 ---shibuya
 --hachiyama
-END
-    html2 = <<END
+
+;;; out
 <div class="section">
-\t<ul>
-\t\t<li>komono
-\t\t<ul>
-\t\t\t<li>kyoto
-\t\t\t<ul>
-\t\t\t\t<li>shibuya</li>
-\t\t\t</ul>
-\t\t\t</li>
-\t\t\t<li>hachiyama</li>
-\t\t</ul>
-\t\t</li>
-\t</ul>
+	<ul>
+		<li>komono
+		<ul>
+			<li>kyoto
+			<ul>
+				<li>shibuya</li>
+			</ul>
+			</li>
+			<li>hachiyama</li>
+		</ul>
+		</li>
+	</ul>
 </div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    # ul2
-    text = <<END
--komono
---kyoto
----shibuya
---hachiyama
-END
-    @p.parse(text)
-    html = @p.html
-
-    html2 = <<END
-<div class="section">
-<div class="section">
-\t<ul>
-\t\t<li>komono
-\t\t<ul>
-\t\t\t<li>kyoto
-\t\t\t<ul>
-\t\t\t\t<li>shibuya</li>
-\t\t\t</ul>
-\t\t\t</li>
-\t\t\t<li>hachiyama</li>
-\t\t</ul>
-\t\t</li>
-\t</ul>
-</div>
-END
-
-    # ol
-    text = <<END
+===
+;;; in
 +komono
 +kyoto
 +shibuya
-END
-    html2 = <<END
-<div class="section">
-\t<ol>
-\t\t<li>komono</li>
-\t\t<li>kyoto</li>
-\t\t<li>shibuya</li>
-\t</ol>
-</div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    # pre
-    text = <<END
+;;; out
+<div class="section">
+	<ol>
+		<li>komono</li>
+		<li>kyoto</li>
+		<li>shibuya</li>
+	</ol>
+</div>
+
+===
+;;; in
 >|
 #!/usr/bin/perl
 |<
-END
-    html2 = <<END
+
+;;; out
 <div class="section">
-\t<pre>
+	<pre>
 #!/usr/bin/perl
 </pre>
 </div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    # superpre
-    text = <<END
+===
+;;; in
 >||
 html starts with <html>.
 ||<
-END
-    html2 = <<END
+
+;;; out
 <div class="section">
-\t<pre class="hatena-super-pre">
+	<pre class="hatena-super-pre">
 html starts with &lt;html&gt;.
 </pre>
 </div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    # table
-    text = <<END
+===
+;;; in
 |*Lang|*Module|
 |Perl|Text::Hatena|
-END
-    html2 = <<END
-<div class="section">
-\t<table>
-\t\t<tr>
-\t\t\t<th>Lang</th>
-\t\t\t<th>Module</th>
-\t\t</tr>
-\t\t<tr>
-\t\t\t<td>Perl</td>
-\t\t\t<td>Text::Hatena</td>
-\t\t</tr>
-\t</table>
-</div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    # tagline
-    text = <<END
+;;; out
+<div class="section">
+	<table>
+		<tr>
+			<th>Lang</th>
+			<th>Module</th>
+		</tr>
+		<tr>
+			<td>Perl</td>
+			<td>Text::Hatena</td>
+		</tr>
+	</table>
+</div>
+
+===
+;;; in
 ><div>no paragraph line</div><
 paragraph line
-END
-    html2 = <<END
-<div class="section">
-\t<div>no paragraph line</div>
-\t<p>paragraph line</p>
-</div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chop!
-    assert_equal(html2, html)
 
-    # tag
-    text = <<END
+;;; out
+<div class="section">
+	<div>no paragraph line</div>
+	<p>paragraph line</p>
+</div>
+
+===
+;;; in
 ><blockquote>
 no paragraph
 lines
 </blockquote><
 paragraph
 lines
-END
-    html2 = <<END
-<div class="section">
-\t<blockquote>
-\t\tno paragraph
-\t\tlines
-\t</blockquote>
-\t<p>paragraph</p>
-\t<p>lines</p>
-</div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    text = <<END
+;;; out
+<div class="section">
+	<blockquote>
+		no paragraph
+		lines
+	</blockquote>
+	<p>paragraph</p>
+	<p>lines</p>
+</div>
+
+===
+;;; in
 Here is the way to make link.
 >||
 http://www.hatena.ne.jp/
 id:jkondo
 ||<
 bye.
-END
-    html2 = <<END
+
+;;; out
 <div class="section">
-\t<p>Here is the way to make link.</p>
-\t<pre class="hatena-super-pre">
+	<p>Here is the way to make link.</p>
+	<pre class="hatena-super-pre">
 http://www.hatena.ne.jp/
 id:jkondo
 </pre>
-\t<p>bye.</p>
+	<p>bye.</p>
 </div>
-END
-    @p.parse(text)
-    html = @p.html
-    html2.chomp!
-    assert_equal(html2, html)
 
-    # footnote
-    text = <<END
+===
+;;; in .strip
 GNU((GNU Is Not Unix)) is not unix.
-END
-    html2 = <<END
+
+;;; out
 <div class="section">
-\t<p>GNU<span class="footnote"><a href="http://d.hatena.ne.jp/jkondo/20050906#f1" title="GNU Is Not Unix" name="fn1">*1</a></span> is not unix.</p>
+	<p>GNU<span class="footnote"><a href="http://d.hatena.ne.jp/jkondo/20050906#f1" title="GNU Is Not Unix" name="fn1">*1</a></span> is not unix.</p>
 </div>
 <div class="footnote">
-\t<p class="footnote"><a href="http://d.hatena.ne.jp/jkondo/20050906#fn1" name="f1">*1</a>: GNU Is Not Unix</p>
+	<p class="footnote"><a href="http://d.hatena.ne.jp/jkondo/20050906#fn1" name="f1">*1</a>: GNU Is Not Unix</p>
 </div>
-END
-     @p.parse(text)
-     html = @p.html
-     assert_equal(html2, html)
-  end
-end
