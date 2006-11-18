bf96af42d66737a299bd704e396adcc97e747159
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2006-11-18 18:55:55 +0000

    * t/test_text_hatena_autolink.rb: Test::Baseを使った。

--- t/test_text_hatena_autolink.rb
+++ t/test_text_hatena_autolink.rb
@@ -1,186 +1,165 @@
 # -*- mode: ruby; coding: utf-8 -*-
-
-$:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
-
-require "test/unit"
+require "t/test_helper"
 require "text/hatena/auto_link"
 
-class Test_Text_Hatena_AutoLink < Test::Unit::TestCase
-  # 01_autolink_compile.t
-  def test_01
-    assert_instance_of(Class, Text::Hatena::AutoLink)
+class TextHatenaAutoLinkTest < Test::Base
+  filters %w(.strip)
+  run_equal :out, :in
+
+  def setup
+    @options = {}
   end
 
-  # 02_autolink_text.t
-  def test_02
-    t = Text::Hatena::AutoLink.new
-    text = "Hi, this is a simple text."
-    html = t.parse(text)
-    assert_equal(text, html)
+  def hatenaize(value)
+    parser = Text::Hatena.new({ :baseuri => 'http://d.hatena.ne.jp/jkondo',
+                                :permalink => 'http://d.hatena.ne.jp/jkondo/20050906',
+                                :ilevel => 0,
+                                :ivalidnode => [],
+                                :sectionanchor => 'sa' }.merge(@options))
+    parser.parse(value)
+    parser.html
   end
 
-  # 03_autolink_http.t
-  def test_03
-    t = Text::Hatena::AutoLink::HTTP.new
-    pat = t.pattern
-
-    text = "This is our site. http://www.hatena.ne.jp/"
-    html = text.gsub(pat) do |m|
-      t.parse(m)
-    end
-
-    html2 = 'This is our site. <a href="http://www.hatena.ne.jp/">http://www.hatena.ne.jp/</a>'
-    assert_equal(html2, html)
-
-    t = Text::Hatena::AutoLink::HTTP.new({:a_target => "_blank"})
-
-    text = "This is our site. http://www.hatena.ne.jp/"
-    html = text.gsub(pat) do |m|
-      t.parse(m)
-    end
-
-    html2 = 'This is our site. <a href="http://www.hatena.ne.jp/" target="_blank">http://www.hatena.ne.jp/</a>'
-    assert_equal(html2, html)
-
-    t = Text::Hatena::AutoLink::HTTP.new
-    text = "[http://www.hatena.ne.jp/images/top/h1.gif:image]"
-    html = text.gsub(/(#{pat})/) do
-      t.parse($1)
-    end
-
-    html2 = '<a href="http://www.hatena.ne.jp/images/top/h1.gif"><img src="http://www.hatena.ne.jp/images/top/h1.gif" alt="http://www.hatena.ne.jp/images/top/h1.gif" class="hatena-http-image"></a>'
-    assert_equal(html2, html)
-
-    t = Text::Hatena::AutoLink::HTTP.new({:a_target => "_blank"})
-    text = "[http://www.hatena.ne.jp/images/top/h1.gif:image:w150]"
-    html = text.gsub(/(#{pat})/) do
-      t.parse($1)
-    end
-
-    html2 = '<a href="http://www.hatena.ne.jp/images/top/h1.gif" target="_blank"><img src="http://www.hatena.ne.jp/images/top/h1.gif" alt="http://www.hatena.ne.jp/images/top/h1.gif" class="hatena-http-image" width="150"></a>'
-    assert_equal(html2, html)
-
-    text = '[http://www.hatena.ne.jp/mobile/:barcode]'
-    html = text.gsub(/(#{pat})/) do
-      t.parse($1)
-    end
-
-    html2 = '<a href="http://www.hatena.ne.jp/mobile/" target="_blank"><img src="http://d.hatena.ne.jp/barcode?str=http%3a%2f%2fwww%2ehatena%2ene%2ejp%2fmobile%2f" class="barcode" alt="http://www.hatena.ne.jp/mobile/"></a>'
-    assert_equal(html2, html)
-
-    text = 'This is our secure site. https://www.hatena.ne.jp/'
-    html = text.gsub(/(#{pat})/) do
-      t.parse($1)
-    end
-
-    html2 = 'This is our secure site. <a href="https://www.hatena.ne.jp/" target="_blank">https://www.hatena.ne.jp/</a>'
-    assert_equal(html2, html)
-
-    text = 'This is our site. [http://www.hatena.ne.jp/:title=Hatena]'
-    html = text.gsub(/(#{pat})/) do
-      t.parse($1)
-    end
-
-    html2 = 'This is our site. <a href="http://www.hatena.ne.jp/" target="_blank">Hatena</a>'
-    assert_equal(html2, html)
-
-    text = 'This is our site. [http://www.hatena.ne.jp/:title]'
-    html = text.gsub(/(#{pat})/) do
-      t.parse($1)
-    end
-
-    html2 = 'This is our site. <a href="http://www.hatena.ne.jp/" target="_blank">はてな</a>'
-    assert_equal(html2, html)
-
-    text = 'This is our site. [http://www.hatena.ne.jp/:detail]'
-    html = text.gsub(/(#{pat})/) do
-      t.parse($1)
-    end
-
-    html2 = 'This is our site. <div class="hatena-http-detail"><p class="hatena-http-detail-url"><a href="http://www.hatena.ne.jp/" target="_blank">http://www.hatena.ne.jp/</a></p><p class="hatena-http-detail-title">はてな</p></div>'
-    assert_equal(html2, html)
+  def do_auto_link(value)
+    parser = Text::Hatena::AutoLink.new(@options)
+    parser.parse(value)
   end
 
-  # 04_autolink_ftp.t
-  def test_04
-    t = Text::Hatena::AutoLink::FTP.new
-    pat = t.pattern
-
-    text = 'This is our files. ftp://www.hatena.ne.jp/'
-    html = text.gsub(/(#{pat})/) do
-      t.parse($1)
-    end
-
-    html2 = 'This is our files. <a href="ftp://www.hatena.ne.jp/">ftp://www.hatena.ne.jp/</a>'
-    assert_equal(html2, html)
+  def a_target_blank(value)
+    @options[:a_target] = '_blank'
+    value
   end
 
-  # 12_autolink_unbracket.t
-  def test_12
-    t = Text::Hatena::AutoLink::Unbracket.new
-    pat = t.pattern
-
-    text = 'I don\'t want to link []id:jkondo[].'
-    html = text.gsub(/(#{pat})/) do
-      t.parse($1)
-    end
-    html2 = 'I don\'t want to link id:jkondo.'
-    assert_equal(html2, html)
+  def scheme_option(value)
+    @options[:scheme_option] = {:id => {:a_target => nil}}
+    value
   end
 
-  # 99_autolink.t
-  def test_99
-    t = Text::Hatena::AutoLink.new
+  def autolink_option(value)
+    @options[:autolink_option] = {
+      :a_target => '_top',
+      :scheme_option => {
+        :id => {
+          :a_target => '_blank',
+        }
+      }
+    }
+    value
+  end
+end
 
-    text = 'Here is my album. f:id:sample'
-    html = t.parse(text)
-    html2 = 'Here is my album. <a href="http://f.hatena.ne.jp/sample/">f:id:sample</a>'
-    assert_equal(html2, html)
+__END__
+=== 02_autolink_text.t
+--- in do_auto_link
+Hi, this is a simple text.
+--- out
+Hi, this is a simple text.
 
-    text = 'Hatena news. [google:news:はてな]'
-    html = t.parse(text)
-    html2 = 'Hatena news. <a href="http://news.google.com/news?q=%e3%81%af%e3%81%a6%e3%81%aa&ie=utf-8&oe=utf-8">google:news:はてな</a>'
-    assert_equal(html2, html)
+=== 03_autolink_http.t
+--- in do_auto_link
+This is our site. http://www.hatena.ne.jp/
+--- out
+This is our site. <a href="http://www.hatena.ne.jp/">http://www.hatena.ne.jp/</a>
 
-    base = 'http://d.hatena.ne.jp/jkondo'
-    parma = 'http://d.hatena.ne.jp/jkondo/20050906'
-    sa = 'sa'
+=== 03_autolink_http.t
+--- in a_target_blank do_auto_link
+This is our site. http://www.hatena.ne.jp/
+--- out
+This is our site. <a href="http://www.hatena.ne.jp/" target="_blank">http://www.hatena.ne.jp/</a>
 
-    p = Text::Hatena.new(:baseuri => base,
-                         :permalink => parma,
-                         :ilevel => 0,
-                         :ivalidnode => [],
-                         :sectionanchor => sa)
+=== 03_autolink_http.t
+--- in do_auto_link
+[http://www.hatena.ne.jp/images/top/h1.gif:image]
+--- out
+<a href="http://www.hatena.ne.jp/images/top/h1.gif"><img src="http://www.hatena.ne.jp/images/top/h1.gif" alt="http://www.hatena.ne.jp/images/top/h1.gif" class="hatena-http-image"></a>
 
-    text = <<END
+=== 03_autolink_http.t
+--- in a_target_blank do_auto_link
+[http://www.hatena.ne.jp/images/top/h1.gif:image:w150]
+--- out
+<a href="http://www.hatena.ne.jp/images/top/h1.gif" target="_blank"><img src="http://www.hatena.ne.jp/images/top/h1.gif" alt="http://www.hatena.ne.jp/images/top/h1.gif" class="hatena-http-image" width="150"></a>
+
+=== 03_autolink_http.t
+--- in a_target_blank do_auto_link
+[http://www.hatena.ne.jp/mobile/:barcode]
+--- out
+<a href="http://www.hatena.ne.jp/mobile/" target="_blank"><img src="http://d.hatena.ne.jp/barcode?str=http%3a%2f%2fwww%2ehatena%2ene%2ejp%2fmobile%2f" class="barcode" alt="http://www.hatena.ne.jp/mobile/"></a>
+
+=== 03_autolink_http.t
+--- in a_target_blank do_auto_link
+This is our secure site. https://www.hatena.ne.jp/
+--- out
+This is our secure site. <a href="https://www.hatena.ne.jp/" target="_blank">https://www.hatena.ne.jp/</a>
+
+=== 03_autolink_http.t
+--- in a_target_blank do_auto_link
+This is our site. [http://www.hatena.ne.jp/:title=Hatena]
+--- out
+This is our site. <a href="http://www.hatena.ne.jp/" target="_blank">Hatena</a>
+
+=== 03_autolink_http.t
+--- in a_target_blank do_auto_link
+This is our site. [http://www.hatena.ne.jp/:title]
+--- out
+This is our site. <a href="http://www.hatena.ne.jp/" target="_blank">はてな</a>
+
+=== 03_autolink_http.t
+--- in a_target_blank do_auto_link
+This is our site. [http://www.hatena.ne.jp/:detail]
+--- out
+This is our site. <div class="hatena-http-detail"><p class="hatena-http-detail-url"><a href="http://www.hatena.ne.jp/" target="_blank">http://www.hatena.ne.jp/</a></p><p class="hatena-http-detail-title">はてな</p></div>
+
+=== 04_autolink_ftp.t
+--- in do_auto_link
+This is our files. ftp://www.hatena.ne.jp/
+--- out
+This is our files. <a href="ftp://www.hatena.ne.jp/">ftp://www.hatena.ne.jp/</a>
+
+=== 05_autolink_mailto.t
+--- SKIP
+--- in do_autolink
+send me a mail mailto:info@example.com
+--- out
+send me a mail <a href="mailto:info@example.com">mailto:info@example.com</a>
+
+=== 12_autolink_unblacket.t
+--- in do_auto_link
+I don\'t want to link []id:jkondo[].
+--- out
+I don\'t want to link id:jkondo.
+
+=== 99_autolink.t
+--- in do_auto_link
+Here is my album. f:id:sample
+--- out
+Here is my album. <a href="http://f.hatena.ne.jp/sample/">f:id:sample</a>
+
+=== 99_autolink.t
+--- in do_auto_link
+Hatena news. [google:news:はてな]
+--- out
+Hatena news. <a href="http://news.google.com/news?q=%e3%81%af%e3%81%a6%e3%81%aa&ie=utf-8&oe=utf-8">google:news:はてな</a>
+
+=== 99_autolink.t
+--- in hatenaize
 *Hi
 This is my blog.
 http://d.hatena.ne.jp/jkondo/
-END
-
-    html2 = <<END
+--- out
 <div class="section">
 	<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#p1" name="p1"><span class="sanchor">sa</span></a> Hi</h3>
 	<p>This is my blog.</p>
 	<p><a href="http://d.hatena.ne.jp/jkondo/">http://d.hatena.ne.jp/jkondo/</a></p>
 </div>
-END
 
-    p.parse(text)
-    html = p.html
-    html2.chomp!
-
-    assert_equal(html2, html)
-
-    text = <<END
+=== 99_autolink.t
+--- in hatenaize
 *Hi
-This is our site. [http://www.hatena.ne.jp/:detail] Prese visit.
+This is our site. [http://www.hatena.ne.jp/:detail] Please visit.
 ><blockquote>
 [http://www.hatena.ne.jp/:detail]
 </blockquote><
-END
-
-    html2 = <<END
+--- out
 <div class="section">
 	<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#p1" name="p1"><span class="sanchor">sa</span></a> Hi</h3>
 	<p>This is our site. </p><div class="hatena-http-detail"><p class="hatena-http-detail-url"><a href="http://www.hatena.ne.jp/">http://www.hatena.ne.jp/</a></p><p class="hatena-http-detail-title">はてな</p></div><p> Please visit.</p>
@@ -188,45 +167,49 @@ END
 		<div class="hatena-http-detail"><p class="hatena-http-detail-url"><a href="http://www.hatena.ne.jp/">http://www.hatena.ne.jp/</a></p><p class="hatena-http-detail-title">はてな</p></div>
 	</blockquote>
 </div>
-END
 
-    p.parse(text)
-    html = p.html
-    html2.chomp!
-
-    assert(html2, html)
-
-    text = <<END
+=== 99_autolink.t
+--- in hatenaize
 *Introducing my book.
 Here is my book.
 [asin:4798110523:image]
-END
-    html2 = <<END
+--- out
 <div class="section">
 	<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#p1" name="p1"><span class="sanchor">sa</span></a> Introducing my book.</h3>
 	<p>Here is my book.</p>
 	<p><a href="http://d.hatena.ne.jp/asin/4798110523"><img src="http://images-jp.amazon.com/images/P/4798110523.09.MZZZZZZZ.jpg" alt="「へんな会社」のつくり方" title="「へんな会社」のつくり方" class="asin"></a></p>
 </div>
-END
 
-    p.parse(text)
-    html = p.html
-    html2.chomp!
-    assert_equal(html2, html)
+=== 99_autolink.t
+--- in hatenaize
+*Introducing my book.
+Here is my book.
+[asin:4798110523:image]
+--- out
+<div class="section">
+	<h3><a href="http://d.hatena.ne.jp/jkondo/20050906#p1" name="p1"><span class="sanchor">sa</span></a> Introducing my book.</h3>
+	<p>Here is my book.</p>
+	<p><a href="http://d.hatena.ne.jp/asin/4798110523"><img src="http://images-jp.amazon.com/images/P/4798110523.09.MZZZZZZZ.jpg" alt="「へんな会社」のつくり方" title="「へんな会社」のつくり方" class="asin"></a></p>
+</div>
 
-    text = <<END
+=== 99_autolink.t
+--- in hatenaize
 Hello, []id:jkondo[].
-END
-
-    html2 = <<END
+--- out
 <div class="section">
 	<p>Hello, id:jkondo.</p>
 </div>
-END
 
-    p.parse(text)
-    html = p.html
-    html2.chomp!
-    assert_equal(html2, html)
-  end
-end
+=== 99_autolink.t
+--- in a_target_blank scheme_option do_auto_link
+http://www.hatena.ne.jp/ &gt; id:jkondo
+--- out
+<a href="http://www.hatena.ne.jp/" target="_blank">http://www.hatena.ne.jp/</a> &gt; <a href="/jkondo/">id:jkondo</a>
+
+=== 99_autolink.t
+--- in autolink_option hatenaize
+http://www.hatena.ne.jp/ &gt; id:jkondo
+--- out
+<div class="section">
+	<p><a href="http://www.hatena.ne.jp/" target="_top">http://www.hatena.ne.jp/</a> &gt; <a href="/jkondo/" target="_blank">id:jkondo</a></p>
+</div>
