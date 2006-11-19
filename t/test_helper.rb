e38c364f05db1c244cde1e021d443709711631e9
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2006-11-19 15:33:11 +0000

    * t/test_helper.rb (Test::Base::PASUTHROUGH_EXCEPTIONS): エラー回避の為。

--- t/test_helper.rb
+++ t/test_helper.rb
@@ -4,5 +4,10 @@ rescue LoadError
   require "rubygems"
   require "test/base"
 end
+module Test
+  class Base
+    PASSTHROUGH_EXCEPTIONS = []
+  end
+end
 $:.unshift(File.dirname(__FILE__) + "/../lib")
 require "text/hatena"
