1882c5a3e59bb11a74f6c0449f5d6fe23c2dd7a4
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2006-12-24 05:11:15 +0000

    * lib/text/hatena/auto_link/google.rb (Text::Hatena::AutoLink::Google): 移植完了。

--- lib/text/hatena/auto_link/google.rb
+++ lib/text/hatena/auto_link/google.rb
@@ -11,12 +11,19 @@ module Text
         end
 
         def parse(text, opts)
-          return nil unless @@pattern =~ text
+          return unless @@pattern =~ text
           type, word = $1 || "", $2
           if type.downcase == 'news'
             return sprintf('<a href="http://news.google.com/news?q=%s&ie=utf-8&oe=utf-8"%s>google:%s:%s</a>',
                            html_encode(word), @a_target_string,
                            type, word)
+          elsif /^images?$/i =~ type
+            return sprintf('<a href="http://images.google.com/images?q=%s&ie=utf-8&oe=utf-8"%s>google:%s:%s</a>',
+                           html_encode(word), @a_target_string,
+                           type, word)
+          else
+            return sprintf('<a href="http://www.google.com/search?q=%s&ie=utf-8&oe=utf-8"%s>google:%s</a>',
+                           html_encode(word), @a_target_string, word)
           end
         end
       end
