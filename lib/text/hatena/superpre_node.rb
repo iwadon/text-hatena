a53136b32df34b835e45f6cac1b1e96e1dc72924
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2008-01-23 02:59:46 +0000

    * lib/text/hatena/superpre_node.rb (Text::Hatena::Formatter): コード整形を行うクラス。VimColorクラスのラッパー。
    * lib/text/hatena/superpre_node.rb (Text::Hatena::SuperpreNode#parse): Text::Hatena::PreNode#parseからコピー。かつVimColorに対応しました。

--- lib/text/hatena/superpre_node.rb
+++ lib/text/hatena/superpre_node.rb
@@ -3,11 +3,58 @@ require "text/hatena/pre_node"
 module Text
   class Hatena
     class SuperpreNode < PreNode
+      begin
+        begin
+          require 'vimcolor'
+        rescue LoadError
+          require 'rubygems'
+          require 'vimcolor'
+        end
+        Formatter = ::VimColor
+      rescue LoadError
+        class Formatter
+          def run(str, filetype, formatter_class, *formatter_args)
+            str
+          end
+        end
+      end
+
       def init
-        @pattern = /^>\|\|$/
+        @pattern = /^>\|([^\|]*)\|$/
         @endpattern = /^\|\|<$/
         @startstring = '<pre class="hatena-super-pre">'
         @endstring = "</pre>"
+        @formatter = Formatter.new
+      end
+
+      def parse
+        c = @context
+        return unless @pattern =~ c.nextline
+        filetype = $1
+        c.shiftline
+        t = "\t" * @ilevel
+        c.htmllines(t + @startstring)
+        x = ""
+        txt = ''
+        while c.hasnext
+          l = c.nextline
+          if @endpattern =~ l
+            x = $` || ""
+            c.shiftline
+            break
+          end
+          #c.htmllines(escape_pre(c.shiftline))
+          txt << c.shiftline << "\n"
+        end
+        unless txt.empty?
+          options = {:encoding => 'utf-8'}
+          options[:filetype] = filetype if filetype
+          @formatter.run(txt, options, :html).each_line do |line|
+            line.chomp!
+            c.htmllines(line)
+          end
+        end
+        c.htmllines("#{x}#{@endstring}")
       end
 
       def escape_pre(s)
