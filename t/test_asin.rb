d843dfdea4683c6919296a04b644d0a7a199eff0
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2009-05-09 10:04:26 +0000

    * t/test_asin.rb: asin記法の出力結果の変更に対応した。
    * t/test_asin.rb: サンプルを一つ追加。

--- t/test_asin.rb
+++ t/test_asin.rb
@@ -26,14 +26,36 @@ asin:4798110523:detail
   <div class="hatena-asin-detail-info">
   <p class="hatena-asin-detail-title"><a href="http://d.hatena.ne.jp/asin/4798110523">「へんな会社」のつくり方 (NT2X)</a></p>
   <ul>
-    <li><span class="hatena-asin-detail-label">作者:</span><a href="http://d.hatena.ne.jp/keyword/近藤 淳也" class="keyword">近藤 淳也</a></li>
+    <li><span class="hatena-asin-detail-label">作者:</span>
+      <a href="http://d.hatena.ne.jp/keyword/%b6%e1%c6%a3%20%bd%df%cc%e9" class="keyword">近藤 淳也</a>
+    </li>
     <li><span class="hatena-asin-detail-label">出版社/メーカー:</span>
-    <a href="http://d.hatena.ne.jp/keyword/翔泳社" class="keyword">
-      翔泳社
-    </a>
+      <a href="http://d.hatena.ne.jp/keyword/%e6%c6%b1%cb%bc%d2" class="keyword">翔泳社</a>
     </li>
     <li><span class="hatena-asin-detail-label">発売日:</span>2006/02/13</li>
     <li><span class="hatena-asin-detail-label">メディア:</span>単行本</li>
+  </ul>
+</div>
+<div class="hatena-asin-detail-foot"></div>
+</div>
+</p></p>
+</div>
+
+===
+--- input
+asin:B00260G73Q:detail
+--- output
+<div class="section">
+	<p><p><div class="hatena-asin-detail">
+  <a href="http://d.hatena.ne.jp/asin/B00260G73Q"><img src="http://d.hatena.ne.jp/images/hatena_aws.gif" class="hatena-asin-detail-image" alt="たのしいカトリーヌ" title="たのしいカトリーヌ"></a>
+  <div class="hatena-asin-detail-info">
+  <p class="hatena-asin-detail-title"><a href="http://d.hatena.ne.jp/asin/B00260G73Q">たのしいカトリーヌ</a></p>
+  <ul>
+    <li><span class="hatena-asin-detail-label">出版社/メーカー:</span>
+      <a href="http://d.hatena.ne.jp/keyword/%bc%ab%c5%be%bc%d6%c1%cf%b6%c8" class="keyword">自転車創業</a>
+    </li>
+    <li><span class="hatena-asin-detail-label">発売日:</span>2009/05/04</li>
+    <li><span class="hatena-asin-detail-label">メディア:</span>CD-ROM</li>
   </ul>
 </div>
 <div class="hatena-asin-detail-foot"></div>
