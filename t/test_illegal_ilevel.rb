9872d83623b01498e5dfff6cb3c0432ce23c21f1
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2010-04-21 12:21:11 +0900

    * t/test_illegal_ilevel.rb (IllegalIlevelTest#test_s_new): assertionを追加。

--- t/test_illegal_ilevel.rb
+++ t/test_illegal_ilevel.rb
@@ -6,5 +6,10 @@ require "text/hatena"
 class IllegalIlevelTest < Test::Unit::TestCase
   def test_s_new
     hatena = Text::Hatena.new
+    assert_equal(0, hatena.instance_eval("@ilevel"))
+    hatena = Text::Hatena.new({:ilevel => 1})
+    assert_equal(1, hatena.instance_eval("@ilevel"))
+    hatena = Text::Hatena.new({:ilevel => nil})
+    assert_equal(0, hatena.instance_eval("@ilevel"))
   end
 end
